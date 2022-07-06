c2da75f0572b40d7676266d3998725e15031f77c
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-06 20:20:21 +0600

    add google search console

--- src/components/meta/Meta.js
+++ src/components/meta/Meta.js
@@ -7,6 +7,7 @@ const Meta = ({ title, keyword, description }) => (
     <meta name="keywords" content={keyword} />
     <meta name="author" content="Razu Islam" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="4NNGhh2Yr93ykAfqit16o1fNSegQRwQXLMrbopG-xwI" />
     <title>{title}</title>
   </Head>
 );
